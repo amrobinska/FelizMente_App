506f835903729ebda23f3b38f2b606ddf1799ceb
amrobinska <amrobinska@gmail.com>
2022-04-24 22:09:05 +0200

    Database created and added changes to RegistrationActivity

--- app/src/main/java/com/example/felizmente/RegistrationActivity.java
+++ app/src/main/java/com/example/felizmente/RegistrationActivity.java
@@ -3,12 +3,107 @@ package com.example.felizmente;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.felizmente.db.ControladorDB;
+
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class RegistrationActivity extends AppCompatActivity {
+
+    ControladorDB db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+        db = new ControladorDB(this);
+        //getSupportActionBar().hide();
+    }
+
+    public void addUser(View view) throws NoSuchAlgorithmException {
+
+        EditText userNameInput = findViewById(R.id.userName);
+        String user = userNameInput.getText().toString().toLowerCase();
+
+        EditText emailInput = findViewById(R.id.userEmailAddress);
+        String email = emailInput.getText().toString().toLowerCase();
+
+        EditText passwordInput = findViewById(R.id.userPassword);
+        String password = passwordInput.getText().toString();
+
+        boolean exists = db.userExists(user);
+
+        if(!user.isEmpty() && !email.isEmpty() && !password.isEmpty()){
+            if(!validateEmail(email)) {
+                emailInput.setError("Formato de email erróneo.");
+                emailInput.setText("");
+            } else if(!validatePassword(password)){
+                passwordInput.setError("La contraseña debe tener entre 8 y 20 caracteres e " +
+                        "incluir al menos un número, una mayúscula, una minúscula y uno de estos " +
+                        "caracteres: @#$%");
+                passwordInput.setText("");
+            } else {
+                if(!exists){
+                    password = hashPassword(password);
+                    db.addUser(user, email, password);
+                    Toast.makeText(this, "Usuario registrado correctamente.", Toast.LENGTH_LONG).show();
+                } else {
+                    userNameInput.setText("");
+                    emailInput.setText("");
+                    passwordInput.setText("");
+                    userNameInput.requestFocus();
+                    Toast.makeText(this, "Ese nombre de usuario ya existe.", Toast.LENGTH_LONG).show();
+                }
+            }
+        } else if(user.isEmpty()){
+            userNameInput.setError("Falta el nombre");
+            userNameInput.requestFocus();
+        } else if(email.isEmpty()){
+            emailInput.setError("Falta el email");
+            emailInput.requestFocus();
+        } else {
+            passwordInput.setError("Falta la contraseña");
+            passwordInput.requestFocus();
+        }
+
+        // ESTARIA GUAY SI PUDIESEMOS AÑADIR UN CHECKBOX PARA MANTERNERSE LOGGEADO O SI LA APP LO HICIESE AUTOMATICAMENTE (COMPROBAR ESTO)
+    }
+
+
+    // copiado de https://java2blog.com/validate-password-java/
+    public boolean validatePassword(String password){
+        String regex = "^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%]).{8,20}$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(password);
+        return matcher.matches();
+    }
+
+    public String hashPassword(String password) throws NoSuchAlgorithmException {
+        SecureRandom random = new SecureRandom();
+        byte[] salt = new byte[16];
+        random.nextBytes(salt);
+
+        MessageDigest md = MessageDigest.getInstance("SHA-512");
+        md.update(salt);
+
+        byte[] hashedPassword = md.digest(password.getBytes(StandardCharsets.UTF_8));
+
+        return new String(hashedPassword);
+    }
+
+    //  Sacado de https://stackoverflow.com/questions/18463848/how-to-tell-if-a-random-string-is-an-email-address-or-something-else
+    public boolean validateEmail(String email){
+        Pattern pattern = Pattern.compile("[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,4}");
+        Matcher mat = pattern.matcher(email);
+
+        return mat.matches();
     }
 }
---
+++ app/src/main/java/com/example/felizmente/db/ControladorDB.java
@@ -0,0 +1,53 @@
+package com.example.felizmente.db;
+
+import android.content.ContentValues;
+import android.content.Context;
+
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class ControladorDB extends SQLiteOpenHelper {
+
+    public ControladorDB(Context context) {
+        super(context, "com.example.felizmente.db", null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("CREATE TABLE USERS (ID_USER INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                "NAME_USER TEXT NOT NULL, EMAIL TEXT NOT NULL, PASS TEXT NOT NULL)");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+    }
+
+    public void addUser(String user, String email, String pass) {
+
+        ContentValues registro = new ContentValues();
+        registro.put("NAME_USER", user);
+        registro.put("EMAIL", email);
+        registro.put("PASS", pass);
+
+        SQLiteDatabase db = this.getWritableDatabase();
+        db.insert("USERS", null, registro);
+        db.close();
+    }
+
+    public boolean userExists(String user) {
+        SQLiteDatabase db = this.getReadableDatabase();
+        String[] args = new String[]{user};
+        Cursor cursor = db.rawQuery("SELECT * FROM USERS WHERE NAME_USER=?", args);
+
+        if (cursor.getCount() > 0) {
+            cursor.close();
+            return true;
+        } else {
+            cursor.close();
+            return false;
+        }
+    }
+
+
+}
