3398753af152213f10ce2e502d5b739c1764aa82
Sarah Amselem <correodesarahamselem@gmail.com>
2022-05-02 19:18:10 +0200

    Worked on menu for music and video

--- app/src/main/java/com/example/felizmente/VideoActivity.java
+++ app/src/main/java/com/example/felizmente/VideoActivity.java
@@ -1,6 +1,7 @@
 package com.example.felizmente;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +11,11 @@ public class VideoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_video);
+    }
+
+    public void goToHumor(View view) {
+    }
+
+    public void goToRelaxation(View view) {
     }
 }
