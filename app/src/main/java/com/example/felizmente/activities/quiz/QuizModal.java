9a9edd86e6584bc2e17fe4b4ccb7db9bfa35d05b
Sarah Amselem <correodesarahamselem@gmail.com>
2022-05-15 17:12:50 +0200

    Lombok installed

--- app/src/main/java/com/example/felizmente/activities/quiz/QuizModal.java
+++ app/src/main/java/com/example/felizmente/activities/quiz/QuizModal.java
@@ -1,8 +1,10 @@
 package com.example.felizmente.activities.quiz;
+
+
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import lombok.Getter;
 
 @AllArgsConstructor
 @NoArgsConstructor
