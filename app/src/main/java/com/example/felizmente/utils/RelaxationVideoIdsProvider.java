3ff7ebe91d127ac9802fca01f26eb478b8e8dd48
Sarah Amselem <correodesarahamselem@gmail.com>
2022-05-16 18:21:57 +0200

    Inserted videos into RelaxationVideoIdsProvider

--- app/src/main/java/com/example/felizmente/utils/RelaxationVideoIdsProvider.java
+++ app/src/main/java/com/example/felizmente/utils/RelaxationVideoIdsProvider.java
@@ -3,15 +3,12 @@ package com.example.felizmente.utils;
 import java.util.Random;
 
 public class RelaxationVideoIdsProvider {
-    private static String[] videoIds = {"6JYIGclVQdw", "LvetJ9U_tVY", "S0Q4gqBUs7c", "zOa-rSM4nms"};
-    private static String[] liveVideoIds = {"hHW1oY26kxQ"};
+    private static String[] videoIds = {"13Sm8MzkAQU","uvXgK3kj4CA","WcXK5Iw8yAk","E4vzsi_HtYA","HQ_hpTK3BCI","PeO0wG6eiDE",
+                                        "iw8hWPYZjjY","Pt_NLtk6MLY","okG6BqDzpTA","=lyO9R4zcrIQ","gAzaeA-ifNE","LBDJ_h-9WfE","5AZmk0rCVnE",
+                                        "65RH4B0z6Gc","Vxqxc022vNY","7XSbmL4d294","tADnCEpbPI8","7H7cTSml5zk","SQBNvKBnQnk","E4vzsi_HtYA"};
     private static Random random = new Random();
 
     public static String getNextVideoId() {
         return videoIds[random.nextInt(videoIds.length)];
-    }
-
-    public static String getNextLiveVideoId() {
-        return liveVideoIds[random.nextInt(liveVideoIds.length)];
     }
 }
